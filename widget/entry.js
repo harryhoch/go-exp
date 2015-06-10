f7fe51ab59fe812fae35d0bf5d71d91820597f03
Harry Hochheiser <harryh@pitt.edu>
2015-06-10 09:19:04 -0400

    first version with new ui widget

--- widget/entry.js
+++ widget/entry.js
@@ -2,7 +2,6 @@ var  $= require('jquery');
 require('tipso');
 require('jquery-ui');
 
-
 var widget = require('./lib/widget');
 
 $(document).ready(function(){
@@ -17,4 +16,29 @@ $(document).ready(function(){
     $("#slider").slider();
 
     $('.title-tipso').tipso();
+
+    bar = $( "<div></div>" )
+	.appendTo( "body" )
+	.progressbar({
+            complete: function( event, data ) {
+		alert( "Callbacks are great!" );
+            }
+	})
+	.bind( "progressbarcomplete", function( event, data ) {
+            alert( "Events bubble and support many handlers for extreme flexibility." );
+            alert( "The progress bar value is " + data.value );
+	});
+    
+    bar.progressbar( "option", "value", 100 );var bar = $( "<div></div>" )
+	.appendTo( "body" )
+	.progressbar({ value: 20 });
+    
+    // Get the current value.
+    alert( bar.progressbar( "value" ) );
+    
+    // Update the value.
+    bar.progressbar( "value", 50 );
+    
+    // Get the current value again.
+    alert( bar.progressbar( "value" ) );
 });
